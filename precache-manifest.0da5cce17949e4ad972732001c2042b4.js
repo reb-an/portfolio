a46beb309dabcb9338c8c644e4da8b9ecb0d9c25
Anescar <reb.anescar@gmail.com>
2020-09-29 23:21:15 -0400

    Portfolio

---
+++ precache-manifest.0da5cce17949e4ad972732001c2042b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94bec8e9f9f63fc435767b1072377432",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a27295e85c2a84307f24",
+    "url": "/portfolio/static/css/main.a5deb081.chunk.css"
+  },
+  {
+    "revision": "707e845f9be3d3c4a1f6",
+    "url": "/portfolio/static/js/2.6a4d7a6c.chunk.js"
+  },
+  {
+    "revision": "94aea53326b4d65781e7862fe72435fd",
+    "url": "/portfolio/static/js/2.6a4d7a6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a27295e85c2a84307f24",
+    "url": "/portfolio/static/js/main.9058d6e9.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
